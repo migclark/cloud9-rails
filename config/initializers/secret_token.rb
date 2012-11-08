2cb7847c76e2bd4f8afae7cde4f258418057ba2e
Miguel Clark <migclark@gmail.com>
2012-11-08 11:36:09 -0500

    installed rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cloud9Rails::Application.config.secret_token = '0ec0e8b454d126ce1f735efb13aab3ad38d7a720ae2a39576a02d1f42bdd121bd88aa5e6bcedc288c4a50215afc266f871942c0466eb54e9eb31175828ecb5dd'
